6e1fde94929a6549bbb37ec36657fcf9bcecb7d1
nmd-2000 <manhdung20112000@gmail.com>
2023-01-09 14:45:06 +0000

    Update lmao

--- src/codetext/utils/utils.py
+++ src/codetext/utils/utils.py
@@ -15,7 +15,7 @@ logger = logging.getLogger('utils')
 logging.basicConfig(level = logging.INFO)
 
 
-SUPPORTED_LANGUAGE = ['python', 'java', 'javascript', 'ruby', 'go', 'c', 'cpp', 'c_sharp', 'php', 'rust']
+SUPPORTED_LANGUAGE = ['python', 'java', 'javascript', 'ruby', 'go', 'c', 'cpp', 'c++', 'c#', 'c_sharp', 'php', 'rust']
 
 
 def build_language(language: str, save_path: str=None):
@@ -23,9 +23,16 @@ def build_language(language: str, save_path: str=None):
     Build tree-sitter language
     
     Args:
-        language (str): java, python, cpp, c-sharp, etc
+        language (str): java, python, cpp, c_sharp, etc
         save_path (str): save path (default create a `/tree-sitter/` dir)
     """
+    language = str(language).lower()
+    if language == 'c#':
+        language = 'c_sharp'
+    elif language == 'c++':
+        language = 'cpp'
+
+    assert language.lower() in SUPPORTED_LANGUAGE, f"Expect {language} in {SUPPORTED_LANGUAGE}"
     if not save_path:
         calling_script_path = Path(inspect.getframeinfo(sys._getframe(1)).filename)
         save_path = calling_script_path.parent
@@ -39,12 +46,12 @@ def build_language(language: str, save_path: str=None):
         os.mkdir(ts_path)
     
     # check `tree-sitter/tree-sitter-<language>`
-    ts_lang_path = os.path.join(ts_path, 'tree-sitter-'+language)
+    ts_lang_path = os.path.join(ts_path, 'tree-sitter-'+language.replace('_', '-'))
     if not os.path.exists(ts_lang_path):
         logger.warning(
-            f"Not found `tree-sitter-{language}`, attempt clone from github to {ts_path}"
+            f"Not found `tree-sitter-{language.replace('_', '-')}`, attempt clone from github to {ts_path}"
         )
-        command = f"cd {ts_path}; git clone https://github.com/tree-sitter/tree-sitter-{language}.git"
+        command = f"cd {ts_path}; git clone https://github.com/tree-sitter/tree-sitter-{language.replace('_', '-')}.git"
         subprocess.Popen(command ,shell=True).wait()
         
         assert os.path.exists(ts_lang_path)==True, f"Unable to find {language} tree-sitter in {ts_path}"
@@ -72,12 +79,12 @@ def parse_code(raw_code: str, language: str='Auto', tree_sitter_path: str=None) 
     # TODO: auto detect language
     if language == 'Auto':
         raise NotImplemented("This feature is underdevelopment")
-    
     language = str(language).lower()
     if language == 'c#':
-        language = 'c-sharp'
+        language = 'c_sharp'
     elif language == 'c++':
         language = 'cpp'
+    assert language in SUPPORTED_LANGUAGE, f"Expect {language} in {SUPPORTED_LANGUAGE}"
     
     if tree_sitter_path:
         load_path = tree_sitter_path
