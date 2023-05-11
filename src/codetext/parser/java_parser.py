e2620e4c9c2be8b8aaf3e60d92dac1a170ee2233
minhna1112 <minhngau2804@gmail.com>
2023-05-11 12:13:15 +0700

    Fix return type extraction for Java

--- src/codetext/parser/java_parser.py
+++ src/codetext/parser/java_parser.py
@@ -103,19 +103,26 @@ class JavaParser(LanguageParser):
         return metadata
 
     @staticmethod
-    def get_function_metadata(function_node, blob: str=None) -> Dict[str, str]:
-        if blob:
-            logger.info('From version `0.0.6` this function will update argument in the API')
+    def get_function_metadata(function_node, blob: str = None) -> Dict[str, str]:
         metadata = {
             'identifier': '',
             'parameters': {},
             'return_type': None
         }
         
+        return_kinds = ["void_type", 
+                        "integral_type",
+                        "floating_point_type",
+                        "boolean_type",
+                        "type_identifier",
+                        "scoped_type_identifier",
+                        "generic_type"]
+        
+
         for child in function_node.children:
             if child.type == 'identifier':
-                metadata['identifier'] = get_node_text(child)
-            elif child.type == 'type_identifier':
+                metadata['identifier'] = get_node_text(child)    
+            elif child.type in return_kinds:
                 metadata['return_type'] = get_node_text(child)
             elif child.type == 'formal_parameters':
                 param_list = get_node_by_kind(child, ['formal_parameter'])  # speed_parameter
@@ -123,5 +130,6 @@ class JavaParser(LanguageParser):
                     param_type = get_node_text(param.child_by_field_name('type'))
                     identifier = get_node_text(param.child_by_field_name('name'))
                     metadata['parameters'][identifier] = param_type
+        
         
         return metadata
